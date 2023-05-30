b8f005d328a3b73393aac53fb6f2390213efc196
Mulia Firmansyah Arief <muliafirmansyah16@gmail.com>
2023-05-30 20:01:01 +0700

    change email receiver when user provider register

--- app/Mail/RegisterProvider.php
+++ app/Mail/RegisterProvider.php
@@ -34,7 +34,7 @@ class RegisterProvider extends Mailable
     {
         return new Envelope(
             subject: 'Register Provider',
-            to: ["address" => "admin@hedgecare.ca"],
+            to: ["address" => "cs@hedgecare.ca"],
         );
     }
 
