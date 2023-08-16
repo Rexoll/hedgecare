478b51d0ca74a03baabcdbc38dbef9403334e89e
kevin azela <108333831+Rexoll@users.noreply.github.com>
2023-08-16 21:23:43 +0700

    update mail format

--- app/Mail/RentAfriendOrderNotification.php
+++ app/Mail/RentAfriendOrderNotification.php
@@ -9,6 +9,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
 
 class RentAfriendOrderNotification extends Mailable
 {
@@ -46,12 +47,12 @@ class RentAfriendOrderNotification extends Mailable
         return new Content(
             view: 'emails.order-notification',
             with: [
-                "order_buyer_name" => $this->order->first_name . ' ' . $this->order->last_name,
-                "service" => 'Rent a friend',
+                "order_buyer_name" => Auth::user()->first_name . ' ' . Auth::user()->last_name,
+                "service" => 'House Keeping',
                 "order_hours" => $this->order->to_hour - $this->order->from_hour,
                 "order_date" => $this->order->created_at->format('F j, Y \a\t g A'),
-                "email" => $this->order->email,
-                "phone" => $this->order->phone_number
+                "email" => Auth::user()->email,
+                "phone" => Auth::user()->phone_number
             ]
         );
     }
