0bc755ed653789a451553a067bdd95ec77205421
Mulia Firmansyah Arief <muliafirmansyah16@gmail.com>
2023-10-07 19:32:45 +0700

    fix: eagerload skill in job board

--- app/Http/Controllers/jobBoard/jobBoardController.php
+++ app/Http/Controllers/jobBoard/jobBoardController.php
@@ -240,7 +240,7 @@ class jobBoardController extends Controller
     public function get(Request $request)
     {
         try {
-            $get = jobBoardOrders::with(['user', 'services.maintenance', 'services.housekeeping', 'services.rentafriend'])->paginate(10);
+            $get = jobBoardOrders::with(['user', 'services.maintenance.skill', 'services.housekeeping.skill', 'services.rentafriend.skill'])->paginate(10);
             return response()->json([
                 'data' => $get,
             ], 200);
