478bd00ce6c7a568cfc2814418754d3d757be505
Mulia Firmansyah Arief <muliafirmansyah16@gmail.com>
2023-10-07 19:39:57 +0700

    fix: eagerload search job board

--- app/Http/Controllers/jobBoard/jobBoardController.php
+++ app/Http/Controllers/jobBoard/jobBoardController.php
@@ -284,7 +284,7 @@ class jobBoardController extends Controller
             $validate_query = $validator_query->validate();
 
             $search = jobBoardOrders::where([["service_name", "LIKE", "%" . ($validate_query["service_name"] ?? '') . "%"]])
-                ->with(['user', 'services.maintenance', 'services.housekeeping', 'services.rentafriend'])
+                ->with(['user', 'services.maintenance.skill', 'services.housekeeping.skill', 'services.rentafriend.skill'])
                 ->orderBy('created_at', 'desc')
                 ->get();
 
