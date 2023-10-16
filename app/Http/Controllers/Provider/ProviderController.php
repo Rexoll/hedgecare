670b86d6154e9c7e57438e9991c5e0d7bdebc93d
Mulia Firmansyah Arief <muliafirmansyah16@gmail.com>
2023-10-16 23:10:17 +0700

    refactor: change file name saved profile provider

--- app/Http/Controllers/Provider/ProviderController.php
+++ app/Http/Controllers/Provider/ProviderController.php
@@ -166,7 +166,7 @@ class ProviderController extends Controller
         });
 
         if ($validate['thumbnail'] ?? null != null) {
-            $file_name = sprintf('thumbnail-provider-%s.png', $id);
+            $file_name = sprintf('%s-thumbnail-provider-%s.png', now(), $id);
             $validate['thumbnail']->storeAs(
                 '/storage/images',
                 $file_name,
