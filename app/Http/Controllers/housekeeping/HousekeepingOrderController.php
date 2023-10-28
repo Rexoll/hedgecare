6f041c37ec47eafaa28d5af831bcc8aeef8f1ccc
kevin azela <108333831+Rexoll@users.noreply.github.com>
2023-10-29 05:35:21 +0700

    Update HousekeepingOrderController.php

--- app/Http/Controllers/housekeeping/HousekeepingOrderController.php
+++ app/Http/Controllers/housekeeping/HousekeepingOrderController.php
@@ -130,7 +130,7 @@ class HousekeepingOrderController extends Controller
             $session = Session::retrieve(['id' => $session_id]);
             $email = $session->customer_details->email;
 
-            if ($session->status == 'complete') {
+            if ($session->status == 'open') {
                 $variable = $session->metadata['product_name'];
 
                 switch ($variable) {
@@ -187,7 +187,7 @@ class HousekeepingOrderController extends Controller
                         $status_code = 400;
                         break;
                 }
-            } elseif ($session->status == 'open') {
+            } elseif ($session->status == 'complete') {
                 $response = (['message' => 'Please complete your payment']);
                 $status_code = 400;
             }
