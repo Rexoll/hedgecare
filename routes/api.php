c000390d06a3fc98224e2b6ef3f7bd0027b0c7d3
kevin azela <108333831+Rexoll@users.noreply.github.com>
2023-11-02 19:49:41 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -121,7 +121,7 @@ Route::prefix("rentAfriend")->group(function () {
 });
 
 Route::prefix("providers")->group(function () {
-    Route::middleware('cache.headers:public;max_age=1800;etag')->get("/", [ProviderController::class, "index"]);
+    Route::get("/", [ProviderController::class, "index"]);
     Route::patch("/{id}", [ProviderController::class, "update"]);
     Route::delete("/delete/{id}", [ProviderController::class, "destroy"]);
 });
