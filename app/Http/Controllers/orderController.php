d1a73a91d77c36c42d57420f9e4dcb5f0b3bc46a
kevin azela <108333831+Rexoll@users.noreply.github.com>
2023-11-08 23:41:23 +0700

    Update orderController.php

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -314,6 +314,7 @@ class orderController extends Controller
                     break;
 
                 case 'other':
+                    dd('masuk');
                     $customOrder = CustomOrder::find($order_id);
                     if (is_null($customOrder)) {
                         return response()->json(['message' => 'order_id not found'], 404);
